c3d283fad96731d71f0a042afb6369796f9c4877
Yusuf Adel <yousefadel@paymob.com>
2023-11-04 22:58:56 +0300

    feat: add a cookiecutter template for django project

---
+++ hackmake_backend/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from hackmake_backend.users.models import User
+from hackmake_backend.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
