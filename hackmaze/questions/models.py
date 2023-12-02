c89980045c04d0e2fdc1a09b17a61075da1e2d59
Yusuf Adel <yousefadel@paymob.com>
2023-12-02 04:10:41 +0200

    feat(questions): Add models

--- hackmaze/questions/models.py
+++ hackmaze/questions/models.py
@@ -1,3 +1,30 @@
-from django.db import models  # noqa
+from django.db import models
 
-# Create your models here.
+from hackmaze.rooms.models import Room
+
+
+class Section(models.Model):
+    """
+    Model representing a section that can contain multiple questions.
+    """
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name="sections")
+    order = models.PositiveIntegerField()  # To determine the order of sections in a room
+
+    def __str__(self):
+        return f"{self.title} - {self.room.title}"
+
+
+class Question(models.Model):
+    """
+    Model representing a question within a section.
+    """
+
+    text = models.TextField()
+    section = models.ForeignKey(Section, on_delete=models.CASCADE, related_name="questions")
+    order = models.PositiveIntegerField()  # To determine the order of questions in a section
+
+    def __str__(self):
+        return f"Question {self.order} - {self.section.title}"
