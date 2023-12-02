21da0c6be72a6daf0eef83e2e2eae2b680754c35
Yusuf Adel <yousefadel@paymob.com>
2023-12-02 04:11:14 +0200

    refactor(rooms): Group related models in same app

---
+++ hackmaze/rooms/constants.py
@@ -0,0 +1,21 @@
+MACHINE = "Machine"
+FILE = "File"
+
+TYPE_CHOICES = [
+    (MACHINE, "Machine"),
+    (FILE, "File"),
+]
+
+PUBLIC = "Public"
+PRIVATE = "Private"
+
+VISIBILITY_CHOICES = [
+    (PUBLIC, "Public"),
+    (PRIVATE, "Private"),
+]
+
+DIFFICULTY_CHOICES = [
+    ("easy", "Easy"),
+    ("medium", "Medium"),
+    ("hard", "Hard"),
+]
--- hackmaze/rooms/models.py
+++ hackmaze/rooms/models.py
@@ -3,19 +3,38 @@ from taggit.managers import TaggableManager
 
 from hackmaze.users.models import UserProfile as Author
 
+from .constants import DIFFICULTY_CHOICES, MACHINE, PUBLIC, TYPE_CHOICES, VISIBILITY_CHOICES
+
 
 # Create your models here.
 class Room(models.Model):
-    DIFFICULTY_CHOICES = [
-        ("easy", "Easy"),
-        ("medium", "Medium"),
-        ("hard", "Hard"),
-    ]
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True)
-    visibility = models.BooleanField(default=True)  # True for public, False for private
+    room_type = models.CharField(max_length=10, choices=TYPE_CHOICES, default=MACHINE)
+    visibility = models.CharField(max_length=10, choices=VISIBILITY_CHOICES, default=PUBLIC)
     recommended_video = models.URLField(blank=True, null=True)
     difficulty = models.CharField(max_length=10, choices=DIFFICULTY_CHOICES, default="medium")
     tags = TaggableManager()
+
+    def __str__(self):
+        return self.title
+
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    rooms = models.ManyToManyField(Room)
+
+    def __str__(self):
+        return self.title
+
+
+class Module(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    level = models.IntegerField()
+    is_active = models.BooleanField(default=False)
+    collections = models.ManyToManyField(Collection)
